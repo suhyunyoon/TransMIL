c546d08a7a72a48644d30290bf64ef81a8a74bf2
suhyunyoon <billy555@naver.com>
2022-05-16 06:56:49 +0000

    Add other Methods

--- models/TransMILPositionalAttention.py
+++ models/TransMILPositionalAttention.py
@@ -1,9 +1,32 @@
+import math
+
 import torch
-import torch.nn as nn
+from torch import nn, Tensor
 import torch.nn.functional as F
 import numpy as np
 from nystrom_attention import NystromAttention
 
+
+class PositionalEncoding(nn.Module):
+    
+    def __init__(self, dim: int, dropout: float = 0.1, max_len: int = 30000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, dim, 2) * (-math.log(10000.0) / dim))
+        pe = torch.zeros(max_len, 1, dim)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
 
 class TransLayer(nn.Module):
 
@@ -29,34 +52,33 @@ class TransLayer(nn.Module):
 class PPEG(nn.Module):
     def __init__(self, dim=512):
         super(PPEG, self).__init__()
-        # self.proj = nn.Conv2d(dim, dim, 7, 1, 7//2, groups=dim)
-        # self.proj1 = nn.Conv2d(dim, dim, 5, 1, 5//2, groups=dim)
-        # self.proj2 = nn.Conv2d(dim, dim, 3, 1, 3//2, groups=dim)
-        self.fc = nn.Linear(dim,dim)
-        self.fc1 = nn.Linear(dim, dim)
-        self.fc2 = nn.Linear(dim, dim)
-        self.relu = nn.ReLU()
+        self.proj = nn.Conv2d(dim, dim, 7, 1, 7//2, groups=dim)
+        self.proj1 = nn.Conv2d(dim, dim, 5, 1, 5//2, groups=dim)
+        self.proj2 = nn.Conv2d(dim, dim, 3, 1, 3//2, groups=dim)
+        # self.fc = nn.Linear(dim,dim)
+        # self.fc1 = nn.Linear(dim, dim)
+        # self.fc2 = nn.Linear(dim, dim)
+        # self.relu = nn.ReLU()
 
     def forward(self, x, H, W):
         B, _, C = x.shape
         cls_token, feat_token = x[:, 0], x[:, 1:]
 
-        print()
-        # cnn_feat = feat_token.transpose(1, 2).view(B, C, H, W)
-        # x = self.proj(cnn_feat)+cnn_feat+self.proj1(cnn_feat)+self.proj2(cnn_feat)
-        # x = x.flatten(2).transpose(1, 2)
-        x = self.relu(self.fc(x))
-        x = self.relu(self.fc1(x))
-        x = self.fc2(x)
+        cnn_feat = feat_token.transpose(1, 2).view(B, C, H, W)
+        x = self.proj(cnn_feat)+cnn_feat+self.proj1(cnn_feat)+self.proj2(cnn_feat)
+        x = x.flatten(2).transpose(1, 2)
+        # x_ = self.relu(self.fc(feat_token))
+        # x_ = self.relu(self.fc1(x_))
+        # x_ = self.fc2(x_) + feat_token
 
         x = torch.cat((cls_token.unsqueeze(1), x), dim=1)
         return x
 
 
-class TransMILPPEG(nn.Module):
+class TransMILPositionalAttention(nn.Module):
     def __init__(self, n_classes):
-        super(TransMILPPEG, self).__init__()
-        self.pos_layer = PPEG(dim=512)
+        super(TransMILPositionalAttention, self).__init__()
+        self.pos_layer = PositionalEncoding(dim=512) #PPEG(dim=512)
         self._fc1 = nn.Sequential(nn.Linear(512, 512), nn.ReLU()) # 원래는 1024, 512 
         self.cls_token = nn.Parameter(torch.randn(1, 1, 512))
         self.n_classes = n_classes
@@ -83,10 +105,12 @@ class TransMILPPEG(nn.Module):
         h = torch.cat((cls_tokens, h), dim=1)
 
         #---->Translayer x1
+        h = self.pos_layer(h) # ???
         h = self.layer1(h) #[B, N, 512]
 
         #---->PPEG
-        h = self.pos_layer(h, _H, _W) #[B, N, 512]
+        #h = self.pos_layer(h, _H, _W) #[B, N, 512]
+        h = self.pos_layer(h) #[B, N, 512]
         
         #---->Translayer x2
         h = self.layer2(h) #[B, N, 512]
@@ -103,7 +127,7 @@ class TransMILPPEG(nn.Module):
 
 if __name__ == "__main__":
     data = torch.randn((1, 6000, 1024)).cuda()
-    model = TransMILPPEG(n_classes=2).cuda()
+    model = TransMILPositionalAttention(n_classes=2).cuda()
     print(model.eval())
     results_dict = model(data = data)
     print(results_dict)
